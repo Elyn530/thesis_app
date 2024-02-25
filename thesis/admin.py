da2862bef813f8c58dd432bed61a5caff0b935ec
Elyn530 <54169594+Elyn530@users.noreply.github.com>
2024-02-25 07:48:13 -0900

    Delete thesis directory

--- thesis/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import thesis
-
-class ThesisAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'department', 'publish', 'adviser')  
-    search_fields = ['title', 'author', 'department', 'publish', 'adviser']  # Specify the fields to be searchable
-
-admin.site.register(thesis, ThesisAdmin)
