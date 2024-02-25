8398373eaa229fec2a44d01a5b02aa632148b3fa
Elyn530 <54169594+Elyn530@users.noreply.github.com>
2024-02-25 07:49:35 -0900

    Delete thesis_app directory

--- thesis_app/thesis/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Thesis
-
-def thesis_list(request):
-    theses = Thesis.objects.all()
-    return render(request, 'thesis_app/thesis_list.html', {'theses': theses})
